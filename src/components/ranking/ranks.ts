bdce6dec676368274dc377b950ca823c25cc3206
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2026-02-06 15:36:16 +0200

    feat: expand ranking system with detailed benefits for each rank

--- src/components/ranking/ranks.ts
+++ src/components/ranking/ranks.ts
@@ -1,25 +1,223 @@
 export const ranks = [
-  { name: 'No Rank', minPoints: 0, maxPoints: 19 },
-  { name: 'New Comer 1', minPoints: 20, maxPoints: 560 },
-  { name: 'New Comer 2', minPoints: 561, maxPoints: 920 },
-  { name: 'New Comer 3', minPoints: 921, maxPoints: 1280 },
-  { name: 'New Comer 4', minPoints: 1281, maxPoints: 1640 },
-  { name: 'New Comer 5', minPoints: 1641, maxPoints: 1999 },
-  { name: 'Contributor 1', minPoints: 2000, maxPoints: 3000 },
-  { name: 'Contributor 2', minPoints: 3001, maxPoints: 4000 },
-  { name: 'Contributor 3', minPoints: 4001, maxPoints: 5000 },
-  { name: 'Contributor 4', minPoints: 5001, maxPoints: 6000 },
-  { name: 'Contributor 5', minPoints: 6001, maxPoints: 6999 },
-  { name: 'Leader 1', minPoints: 7000, maxPoints: 7600 },
-  { name: 'Leader 2', minPoints: 7601, maxPoints: 8200 },
-  { name: 'Leader 3', minPoints: 8201, maxPoints: 8800 },
-  { name: 'Leader 4', minPoints: 8801, maxPoints: 9400 },
-  { name: 'Leader 5', minPoints: 9401, maxPoints: 9999 },
-  { name: 'Ambassador 1', minPoints: 10000, maxPoints: 12000 },
-  { name: 'Ambassador 2', minPoints: 12001, maxPoints: 14000 },
-  { name: 'Ambassador 3', minPoints: 14001, maxPoints: 16000 },
-  { name: 'Ambassador 4', minPoints: 16001, maxPoints: 18000 },
-  { name: 'Ambassador 5', minPoints: 18001, maxPoints: 99999999 },
+  {
+    name: 'No Rank',
+    minPoints: 0,
+    maxPoints: 19,
+    benefit: {
+      name: 'Access to all open source projects',
+      description:
+        'You can contribute on all open source projects on the Zim Developers Labs GitHub organization. Fork the repos and submit pull requests to contribute to the projects and earn points to move up in rank and earn more benefits.',
+    },
+  },
+  {
+    name: 'New Comer 1',
+    minPoints: 20,
+    maxPoints: 560,
+    benefit: {
+      name: 'Ability to get credit for images on the blog (without personal links)',
+      description:
+        'You can get credited for designing images placed on the blog without including personal links.',
+    },
+  },
+  {
+    name: 'New Comer 2',
+    minPoints: 561,
+    maxPoints: 920,
+    benefit: {
+      name: 'Merch Discount',
+      description: 'You can receive a discount on merchandise.',
+    },
+  },
+  {
+    name: 'New Comer 3',
+    minPoints: 921,
+    maxPoints: 1280,
+    benefit: {
+      name: 'One-time resume or portfolio review from executives',
+      description:
+        'If you are unsure or need help with your resume or portfolio, you can request a one-time review from our executives.',
+    },
+  },
+  {
+    name: 'New Comer 4',
+    minPoints: 1281,
+    maxPoints: 1640,
+    benefit: {
+      name: 'One-time career guidance session from executives',
+      description:
+        'If you are looking to get started in tech be it as a developer, designer, or content creator, you can request a one-time career guidance session from our executives.',
+    },
+  },
+  {
+    name: 'New Comer 5',
+    minPoints: 1641,
+    maxPoints: 1999,
+    benefit: {
+      name: '$5 Ad credits per month',
+      description:
+        'Every month you can claim $5 in ad credits on the IBZIM Ads platform (ads.ibzim.com).',
+    },
+  },
+  {
+    name: 'Contributor 1',
+    minPoints: 2000,
+    maxPoints: 3000,
+    benefit: {
+      name: 'Ability to get credit for images on the blog with personal links',
+      description:
+        'You can get credited for designing images placed on the blog with personal links. i.e if you design an image for an article about Next.js, you can include a personal link to your Next.js portfolio or github in the image credit.',
+    },
+  },
+  {
+    name: 'Contributor 2',
+    minPoints: 3001,
+    maxPoints: 4000,
+    benefit: {
+      name: 'Ability to place nofollow relevant personal links in articles',
+      description:
+        'You can place nofollow relevant personal links in articles.',
+    },
+  },
+  {
+    name: 'Contributor 3',
+    minPoints: 4001,
+    maxPoints: 5000,
+    benefit: {
+      name: 'Ability to get a personal product promo article on the blog',
+      description:
+        'You can request to have a personal product promo article published on the blog. The product will be reviewed for relevance and quality before the article is published.',
+    },
+  },
+  {
+    name: 'Contributor 4',
+    minPoints: 5001,
+    maxPoints: 6000,
+    benefit: {
+      name: 'Ability to create paid templates',
+      description:
+        'You can sell templates on zimdevelopers.com. Before a template is approved to be sold, it will be reviewed for quality and relevance and the price will be approved by the executives based on the quality of the template',
+    },
+  },
+  {
+    name: 'Contributor 5',
+    minPoints: 6001,
+    maxPoints: 6999,
+    benefit: {
+      name: '$10 Ad credits per month',
+      description:
+        'Every month you can claim $10 in ad credits on the IBZIM Ads platform (ads.ibzim.com).',
+    },
+  },
+  {
+    name: 'Leader 1',
+    minPoints: 7000,
+    maxPoints: 7600,
+    benefit: {
+      name: 'Group Administrator Privilege',
+      description:
+        'You can request to be added as an administrator in the Zim Developers Labs Whatsapp group and Discord server.',
+    },
+  },
+  {
+    name: 'Leader 2',
+    minPoints: 7601,
+    maxPoints: 8200,
+    benefit: {
+      name: 'Ability to join GitHub organization',
+      description:
+        'You can request to join the GitHub organization (We recommend you set your membership to public).',
+    },
+  },
+  {
+    name: 'Leader 3',
+    minPoints: 8201,
+    maxPoints: 8800,
+    benefit: {
+      name: 'Permission to review and merge pull requests on open source projects',
+      description:
+        'You can review and merge pull requests on open source projects.',
+    },
+  },
+  {
+    name: 'Leader 4',
+    minPoints: 8801,
+    maxPoints: 9400,
+    benefit: {
+      name: 'Viewer access on Vercel dashboard for all projects',
+      description:
+        'You can view the Vercel dashboard for all projects. You can use this benefit to see project analytics and deployment status.',
+    },
+  },
+  {
+    name: 'Leader 5',
+    minPoints: 9401,
+    maxPoints: 9999,
+    benefit: {
+      name: '$25 Ad credits per month',
+      description:
+        'Every month you can claim $25 in ad credits on the IBZIM Ads platform (ads.ibzim.com).',
+    },
+  },
+  {
+    name: 'Ambassador 1',
+    minPoints: 10000,
+    maxPoints: 12000,
+    benefit: {
+      name: 'Access to IBZIM Source Code',
+      description:
+        'You can access the IBZIM source code and contribute to its development and maintainance.',
+    },
+  },
+  {
+    name: 'Ambassador 2',
+    minPoints: 12001,
+    maxPoints: 14000,
+    benefit: {
+      name: 'Access to project admin portals',
+      description:
+        'You can access the admin portals of various projects you have access to.',
+    },
+  },
+  {
+    name: 'Ambassador 3',
+    minPoints: 14001,
+    maxPoints: 16000,
+    benefit: {
+      name: 'Access to all project content studios',
+      description:
+        'You can access all project content studios and contribute to articles and other content for potential paid compensation or revenue share (subject to role availability).',
+    },
+  },
+  {
+    name: 'Ambassador 4',
+    minPoints: 16001,
+    maxPoints: 18000,
+    benefit: {
+      name: 'Ability to place personal dofollow links',
+      description:
+        'You can place personal dofollow links on the blog and in project content. This is a powerful benefit that can help you improve your SEO and drive traffic to your personal projects or portfolio.',
+    },
+  },
+  {
+    name: 'Ambassador 5',
+    minPoints: 18001,
+    maxPoints: 19999,
+    benefit: {
+      name: '$50 Ad credits per month',
+      description:
+        'Every month you can claim $50 in ad credits on the IBZIM Ads platform (ads.ibzim.com).',
+    },
+  },
+  {
+    name: 'Executive',
+    minPoints: 20000,
+    maxPoints: 99999999,
+    benefit: {
+      name: 'Executive Privileges',
+      description:
+        'As an executive, you have access to all non buy in project sourcecode and can also participate in high-level decision making for the Zim Developers Labs community. (Buy in projects are projects that require a buy in fee to access the source code which are rapidrange.shop and peyapeya.com)',
+    },
+  },
 ];
 
 export function getCurrentRank(points: number) {
